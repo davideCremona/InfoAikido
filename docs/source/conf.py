4f9f55eda8bc2552f6859c816784faa4b94ffc4f
davideCremona <davide.cremona@mail.polimi.it>
2023-11-29 14:48:07 +0100

    Change some info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'InfoAikido'
+copyright = '2023, Aikido Buikukai Varese'
+author = 'Aikido Buikukai Varese'
 
 release = '0.1'
 version = '0.1.0'
